23f6db8a6253afb4c055cc91436e759ef697e2cb
muthu2552 <anupriyarshiksha@gmail.com>
2024-08-28 13:07:33 +0530

    Add files via upload

---
+++ san.java
@@ -0,0 +1,18 @@
+public class san {
+    public static void main(String args[])
+    {
+        int a,b,c;
+        System.out.println("value of a and b");
+        c=a+b;
+        System.out.println("add="+c);
+        c=a-b;
+        System.out.println("sub="+c);
+        c=a*b;
+        System.out.println("mul="+c);
+        c=a/b;
+        System.out.println("div="+c);
+        c=a%b;
+        System.out.println("mod="+c);
+    }
+    
+}
